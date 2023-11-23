1c825837e5720fa80fa37e4b29c7fbf7177448e7
Steven <steven@Stevens-MacBook-Air.local>
2023-11-23 18:53:25 -0500

    fixed the prompt

--- server.js
+++ server.js
@@ -28,9 +28,10 @@ app.post("/generate-meal-plan", async (req, res) => {
       mealsPerDay,
       primaryObjective,
     } = req.body;
-
-    const tdee = `Calculate the TDEE for a ${gender}, ${age} years old, with a height of ${height} cm, weight of ${weight} kg, and an activity level of ${activityLevel}/5. Do not show all the details of the calculations of tdee and bmr. Just show the result`;
-    const customPrompt = `Based on the calculated TDEE lets calculate the objectiuve caloric intake and the primary objective of ${primaryObjective}. Based on both create a specific 7-day meal plan, varying each day. Every meal should have all the ingredients in grams. and the macro split. Please consider the following dietary preferences: preferred foods are ${preferredFoods}, disliked foods are ${dislikedFoods}, and allergies include ${allergies}. The plan should be for ${mealsPerDay} meals per day.`;
+    const instuctions =
+      "You are a nutricionist with 30 years of experience, and you give out detailes custom meal plan depending on clients needs. DO ot give any warning as it used unnecessary tokens";
+    const tdee = `Calculate the TDEE for a ${gender}, ${age} years old, with a height of ${height} cm, weight of ${weight} kg, and an activity level of ${activityLevel}/5. Do not show all the details of the calculations of tdee and bmr as it using tokens. Just show the result`;
+    const customPrompt = `Give out the calculated TDEE lets calculate the objectiuve caloric intake and the primary objective of ${primaryObjective}. Based on both create a cusotm 3-7 day meal plan, different for each day. Every meal should have all the ingredients in grams. and the macro split. Please consider the following dietary preferences: preferred foods are ${preferredFoods}, disliked foods are ${dislikedFoods}, and allergies include ${allergies}. The plan should be for ${mealsPerDay} meals per day.`;
 
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
@@ -41,6 +42,7 @@ app.post("/generate-meal-plan", async (req, res) => {
       body: JSON.stringify({
         model: "gpt-4",
         messages: [
+          { role: "user", content: instuctions },
           { role: "user", content: tdee },
           { role: "user", content: customPrompt },
         ],
